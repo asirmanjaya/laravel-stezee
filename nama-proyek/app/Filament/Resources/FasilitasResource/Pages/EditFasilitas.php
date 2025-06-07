728377b00d6c63b8096a572dd0eb961f0980627c
Jay <jaymimin66@gmail.com>
2025-06-08 06:58:20 +0700

    Penambahan route pada Filament ketika create fasilitias otomatis kembali ke List

--- nama-proyek/app/Filament/Resources/FasilitasResource/Pages/EditFasilitas.php
+++ nama-proyek/app/Filament/Resources/FasilitasResource/Pages/EditFasilitas.php
@@ -10,6 +10,11 @@ class EditFasilitas extends EditRecord
 {
     protected static string $resource = FasilitasResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
